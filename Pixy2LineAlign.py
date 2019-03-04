4e4af4d7d08366233222761546a15ad21b4e7ced
Khanh Hoang <kkhanhhoang@outlook.com>
2019-03-04 15:38:07 -0800

    Update Pixy2LineAlign.py

--- Pixy2LineAlign.py
+++ Pixy2LineAlign.py
@@ -31,6 +31,7 @@ if pixy.init()!=0:
     print("Pixy 2 not connected")
     exit()
 
+isFrontLine = (pixy.getUID()==-619097246)
 pixy.change_prog ("line")
 
 class Vector (Structure):
@@ -48,18 +49,35 @@ vectors = VectorArray(100)
 while 1:
   line_get_all_features ()
   v_count = line_get_vectors (100, vectors)
-
-  if v_count > 0:
-    for index in range (0, v_count):
-                        yDifference = vectors[index].m_y1-vectors[index].m_y0
-                        xDifference = vectors[index].m_x1-vectors[index].m_x0
-                        distance = math.hypot(yDifference,xDifference)
-                        angle = math.degrees(math.asin(xDifference/distance))
-                        table.putNumber("LineX0F", vectors[index].m_x0)
-                        table.putNumber("LineY0F", vectors[index].m_y0)
-                        table.putNumber("LineX1F", vectors[index].m_x1)
-                        table.putNumber("LineY1F", vectors[index].m_y1)
-                        table.putNumber("LineAngleF", angle)
-                        print '[VECTOR: INDEX=%d X0=%3d Y0=%3d X1=%3d Y1=%3d ANGLE=%3f]' % (vectors[index].m_index, vectors[index].m_x0, vectors[index].m_y0, vectors[index].m_x1, vectors[index].m_y1, angle)
-                        
+  
+  if(isFrontLine):
+    if v_count > 0:
+        for index in range (0, v_count):
+                            yDifference = vectors[index].m_y1-vectors[index].m_y0
+                            xDifference = vectors[index].m_x1-vectors[index].m_x0
+                            distance = math.hypot(yDifference,xDifference)
+                            angle = math.degrees(math.asin(xDifference/distance))
+                            table.putNumber("LineX0F", vectors[index].m_x0)
+                            table.putNumber("LineY0F", vectors[index].m_y0)
+                            table.putNumber("LineX1F", vectors[index].m_x1)
+                            table.putNumber("LineY1F", vectors[index].m_y1)
+                            table.putNumber("LineAngleF", angle)
+                            print '[VECTOR: INDEX=%d X0=%3d Y0=%3d X1=%3d Y1=%3d ANGLE=%3f]' % (vectors[index].m_index, vectors[index].m_x0, vectors[index].m_y0, vectors[index].m_x1, vectors[index].m_y1, angle)
+    else:
+        table.putNumber("isLineFDetected", false)
+  else:
+    if v_count > 0:
+        for index in range (0, v_count):
+                            yDifference = vectors[index].m_y1-vectors[index].m_y0
+                            xDifference = vectors[index].m_x1-vectors[index].m_x0
+                            distance = math.hypot(yDifference,xDifference)
+                            angle = math.degrees(math.asin(xDifference/distance))
+                            table.putNumber("LineX0R", vectors[index].m_x0)
+                            table.putNumber("LineY0R", vectors[index].m_y0)
+                            table.putNumber("LineX1R", vectors[index].m_x1)
+                            table.putNumber("LineY1R", vectors[index].m_y1)
+                            table.putNumber("LineAngleR", angle)
+                            print '[VECTOR: INDEX=%d X0=%3d Y0=%3d X1=%3d Y1=%3d ANGLE=%3f]' % (vectors[index].m_index, vectors[index].m_x0, vectors[index].m_y0, vectors[index].m_x1, vectors[index].m_y1, angle)
+    else:
+        table.putNumber("isLineRDetected", false)
 
